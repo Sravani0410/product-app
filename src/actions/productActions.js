50fc2d42601659bc306a88290e8037b195e15efe
sravani <sandhyapyla.8@gmail.com>
2024-07-04 18:43:02 +0530

    deploy with json-server using vercel

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-
+const API_URL = 'http://localhost:5000/products';
 export const fetchProducts = () => async dispatch => {
   dispatch({ type: 'FETCH_PRODUCTS_REQUEST' });
   try {
-    const response = await axios.get('http://localhost:5000/products');
+    const response = await axios.get(API_URL);
     dispatch({ type: 'FETCH_PRODUCTS_SUCCESS', payload: response.data });
   } catch (error) {
     dispatch({ type: 'FETCH_PRODUCTS_FAILURE', payload: error.message });
@@ -13,7 +13,7 @@ export const fetchProducts = () => async dispatch => {
 export const selectProduct = id => async dispatch => {
   dispatch({ type: 'FETCH_PRODUCT_REQUEST' });
   try {
-    const response = await axios.get(`http://localhost:5000/products/${id}`);
+    const response = await axios.get(`${API_URL}/${id}`);
     dispatch({ type: 'FETCH_PRODUCT_SUCCESS', payload: response.data });
   } catch (error) {
     dispatch({ type: 'FETCH_PRODUCT_FAILURE', payload: error.message });
@@ -22,7 +22,7 @@ export const selectProduct = id => async dispatch => {
 
 export const addProduct = product => async dispatch => {
   try {
-    const response = await axios.post('http://localhost:5000/products', product);
+    const response = await axios.post(API_URL, product);
     window.location.reload();
     dispatch({ type: 'ADD_PRODUCT', payload: response.data });
   } catch (error) {
@@ -32,7 +32,7 @@ export const addProduct = product => async dispatch => {
 
 export const updateProduct = product => async dispatch => {
   try {
-    const response = await axios.put(`http://localhost:5000/products/${product.id}`, product);
+    const response = await axios.put(`${API_URL}/${product.id}`, product);
     window.location.reload();
     dispatch({ type: 'UPDATE_PRODUCT', payload: response.data });
   } catch (error) {
